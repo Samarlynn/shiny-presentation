dc16e49852f133df28df21a0295bbbc3933ba20b
Samarlynn <cammie.adcock@gmail.com>
2016-06-19 07:04:03 -0600

    added base component information

---
+++ register.js
@@ -0,0 +1,34 @@
+(function() {
+    Polymer({
+        is: 'shiny-presentation',
+
+        created: onElementCreated,
+        attatched: onElementAttatched,
+        ready: onElementReady,
+        detatched: onElementDetatched,
+
+        properties: {
+            element: {
+                type: String,
+                value: '',
+                reflectToAttribute: true,
+            },
+        },
+
+
+    });
+
+    function onElementCreated(){
+    }
+
+    function onElementAttatched(){
+    }
+
+    function onElementReady(){
+    }
+
+    function onElementDetatched(){
+    }
+
+    
+})
